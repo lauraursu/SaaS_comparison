e44a17c4bce874fd68dc4295821f001f81f69e96
Laura Ursu <laura_ursu20@yahoo.com>
2020-08-19 00:42:21 +0300

    css fixes

--- result.php
+++ result.php
@@ -186,7 +186,7 @@ if ($serviceNotPayableMonth){
 
 ?> 
 <!-- for the cards  -->
-<div class="card-deck">
+<div class="card-group">
 
 <?php
 
@@ -236,13 +236,6 @@ if ($finalResult){
 
 <!-- Showing the results in card form -->
 </div>
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title"><?php print($array['name'])?></h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
 </div>
 </div>
     <!--- Footer -->
